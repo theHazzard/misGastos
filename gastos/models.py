b843e6abbf6e801408d2e6f9c6e2479842708f92
Enzo Strongoli <thehazzard@gmail.com>
2012-11-06 12:12:23 -0300

    fourth

--- gastos/models.py
+++ gastos/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Tipo(models.model):
+class Tipo(models.Model):
 	Descripcion = models.CharField(max_length = 100)
 
-class Gasto(models.model):
+class Gasto(models.Model):
 	usuario = models.ForeignKey(User)
 	fecha = models.DateTimeField()
 	tipoid = models.ForeignKey(Tipo)
